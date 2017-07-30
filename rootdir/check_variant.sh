b45db41c1fee91c8bcb8a278224e603b0b6f5609
ishantvivek <ishantvivek1311@gmail.com>
2017-07-30 12:10:07 +0000

    kyleproxx: Remove kylepro ril libs on kyleprods and vice versa

--- rootdir/check_variant.sh
+++ rootdir/check_variant.sh
@@ -7,7 +7,11 @@ if [ "$baseband" == "S7582"* ];
 then
 	mv /system/lib/libbrcm_ril_KYLEPRODS.so /system/lib/libbrcm_ril.so
 	mv /system/lib/libril_KYLEPRODS.so /system/lib/libril.so
+	rm /system/lib/libbrcm_ril_KYLEPRO.so
+	rm /system/lib/libril_KYLEPRO.so
 else
 	mv /system/lib/libbrcm_ril_KYLEPRO.so /system/lib/libbrcm_ril.so
 	mv /system/lib/libril_KYLEPRO.so /system/lib/libril.so
+	rm /system/lib/libbrcm_ril_KYLEPRODS.so
+	rm /system/lib/libril_KYLEPRODS.so
 fi
