73f0f74dce1e57c9044e3936d5635250e6e825dd
ishantvivek <ishantvivek1311@gmail.com>
2017-07-30 12:02:37 +0000

    kyleproxx: Move libs instead of copy

--- rootdir/check_variant.sh
+++ rootdir/check_variant.sh
@@ -5,9 +5,9 @@ baseband=`strings /dev/block/mmcblk0p7 | grep "S7582" | head -1`
 
 if [ "$baseband" == "S7582"* ];
 then
-	cp /system/lib/libbrcm_ril_KYLEPRODS.so /system/lib/libbrcm_ril.so
-	cp /system/lib/libril_KYLEPRODS.so /system/lib/libril.so
+	mv /system/lib/libbrcm_ril_KYLEPRODS.so /system/lib/libbrcm_ril.so
+	mv /system/lib/libril_KYLEPRODS.so /system/lib/libril.so
 else
-	cp /system/lib/libbrcm_ril_KYLEPRO.so /system/lib/libbrcm_ril.so
-        cp /system/lib/libril_KYLEPRO.so /system/lib/libril.so
+	mv /system/lib/libbrcm_ril_KYLEPRO.so /system/lib/libbrcm_ril.so
+	mv /system/lib/libril_KYLEPRO.so /system/lib/libril.so
 fi
