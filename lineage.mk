81b612647e4fb292b37d2427dff1185373a935bc
Real man born on semtember <hieunam03@gmail.com>
2017-09-15 17:45:46 +0700

    Commonization

--- lineage.mk
+++ lineage.mk
@@ -2,12 +2,12 @@
 $(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/kyleprods/device_kyleprods.mk)
+$(call inherit-product, device/samsung/kyleproxx/device_kyleproxx.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kyleprods
-PRODUCT_NAME := lineage_kyleprods
+PRODUCT_NAME := lineage_kyleproxx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := GT-S7582
+PRODUCT_MODEL := GT-S758x
 PRODUCT_CHARACTERISTICS := phone
