9cde3a1d0d486dbc35c21823586b6c5b7d0d2e53
sostk <poudelswastik@gmail.com>
2022-07-19 20:44:49 +1000

    Try to fix healthd

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -17,6 +17,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd-hawaii.cpp
+LOCAL_C_INCLUDES := system/core/healthd/include \
+    system/core/base/include
 LOCAL_MODULE := libhealthd.hawaii
 LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_LIBRARIES := libhealthd.default
