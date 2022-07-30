ade1b64c9d16076d618f812283dcd46e285c6d08
sostk <poudelswastik@gmail.com>
2022-07-30 21:56:32 +1000

    Added missing symbols

---
+++ libsamsung_symbols/tvservice_binder.cpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#include <stdint.h>
+#include <string>
+#define ANDROID_ATOMIC_INLINE
+#include <cutils/atomic.h>
+
+/* status_t VectorImpl::reservedVectorImpl */
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl1Ev();
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl1Ev() {
+    return _ZN7android10VectorImpl19reservedVectorImpl1Ev();
+}
+
+/* status_t VectorImpl::reservedVectorImpl */
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl2Ev();
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl2Ev() {
+    return _ZN7android10VectorImpl19reservedVectorImpl2Ev();
+}
+
+/* status_t VectorImpl::reservedVectorImpl */
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl3Ev();
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl3Ev() {
+    return _ZN7android10VectorImpl19reservedVectorImpl3Ev();
+}
+
+/* status_t VectorImpl::reservedVectorImpl */
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl4Ev();
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl4Ev() {
+    return _ZN7android10VectorImpl19reservedVectorImpl4Ev();
+}
+
+/* status_t VectorImpl::reservedVectorImpl */
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl5Ev();
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl5Ev() {
+    return _ZN7android10VectorImpl19reservedVectorImpl5Ev();
+}
+
+/* status_t VectorImpl::reservedVectorImpl */
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl6Ev();
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl6Ev() {
+    return _ZN7android10VectorImpl19reservedVectorImpl6Ev();
+}
+
+/* status_t VectorImpl::reservedVectorImpl */
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl7Ev();
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl7Ev() {
+    return _ZN7android10VectorImpl19reservedVectorImpl7Ev();
+}
+
+/* status_t VectorImpl::reservedVectorImpl */
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl8Ev();
+extern "C" int _ZN7android10VectorImpl19reservedVectorImpl8Ev() {
+    return _ZN7android10VectorImpl19reservedVectorImpl8Ev();
+}
+
+/* status_t SortedVectorImpl::reservedSortedVectorImpl */
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl1Ev();
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl1Ev() {
+    return _ZN7android16SortedVectorImpl25reservedSortedVectorImpl1Ev();
+}
+
+/* status_t SortedVectorImpl::reservedSortedVectorImpl */
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl2Ev();
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl2Ev() {
+    return _ZN7android16SortedVectorImpl25reservedSortedVectorImpl2Ev();
+}
+
+/* status_t SortedVectorImpl::reservedSortedVectorImpl */
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl3Ev();
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl3Ev() {
+    return _ZN7android16SortedVectorImpl25reservedSortedVectorImpl3Ev();
+}
+
+/* status_t SortedVectorImpl::reservedSortedVectorImpl */
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl4Ev();
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl4Ev() {
+    return _ZN7android16SortedVectorImpl25reservedSortedVectorImpl4Ev();
+}
+
+/* status_t SortedVectorImpl::reservedSortedVectorImpl */
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl5Ev();
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl5Ev() {
+    return _ZN7android16SortedVectorImpl25reservedSortedVectorImpl5Ev();
+}
+
+/* status_t SortedVectorImpl::reservedSortedVectorImpl */
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl6Ev();
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl6Ev() {
+    return _ZN7android16SortedVectorImpl25reservedSortedVectorImpl6Ev();
+}
+
+/* status_t SortedVectorImpl::reservedSortedVectorImpl */
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl7Ev();
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl7Ev() {
+    return _ZN7android16SortedVectorImpl25reservedSortedVectorImpl7Ev();
+}
+
+/* status_t SortedVectorImpl::reservedSortedVectorImpl */
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl8Ev();
+extern "C" int _ZN7android16SortedVectorImpl25reservedSortedVectorImpl8Ev() {
+    return _ZN7android16SortedVectorImpl25reservedSortedVectorImpl8Ev();
+}
+
+/* status_t RefBase::ReferenceConverterBaseE */
+extern "C" int _ZN7android7RefBase14moveReferencesEPvPKvjRKNS_22ReferenceConverterBaseE();
+extern "C" int _ZN7android7RefBase14moveReferencesEPvPKvjRKNS_22ReferenceConverterBaseE() {
+    return _ZN7android7RefBase14moveReferencesEPvPKvjRKNS_22ReferenceConverterBaseE();
+}
