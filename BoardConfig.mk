91f12c0388eb257c1c7db84800b8ba1196ba0ffb
sostk <poudelswastik@gmail.com>
2022-08-01 12:26:50 +1000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,6 +199,7 @@ TARGET_SPECIFIC_HEADER_PATH                 := device/samsung/kyleproxx/include
 TARGET_LD_SHIM_LIBS := \
      /system/lib/egl/libGLES_hawaii.so|libatomicshim.so \
      /system/lib/hw/gralloc.hawaii.so|libatomicshim.so \
+     /system/lib/hw/hwcomposer.hawaii.so|libatomicshim.so \
      /system/lib/hw/camera.hawaii.so|libcamerashim.so \
      /system/lib/hw/audio.primary.hawaii.so|libsamsung_symbols.so \
 
