4839cde568165079afd6bd6944ed4e7c2b9d3c82
sostk <poudelswastik@gmail.com>
2022-08-19 11:39:36 +1000

    Update check_variant.sh

--- rootdir/check_variant.sh
+++ rootdir/check_variant.sh
@@ -10,8 +10,8 @@ then
 	sed -i -e 's/ro.product.device=kyleproxx/ro.product.device=kyleprods/g' /system/build.prop
 	sed -i -e 's/persist.radio.multisim.config=none/persist.radio.multisim.config=dsds/g' /system/build.prop
 	sed -i -e 's/ro.multisim.simslotcount=1/ro.multisim.simslotcount=2/g' /system/build.prop
-	mv /system/vendor/lib/libbrcm_ril_KYLEPRODS.so /system/lib/libbrcm_ril.so
-	mv /system/vendor/lib/libril_KYLEPRODS.so /system/lib/libril.so
+	mv /system/vendor/lib/libbrcm_ril_KYLEPRODS.so /system/vendor/lib/libbrcm_ril.so
+	mv /system/vendor/lib/libril_KYLEPRODS.so /system/vendor/lib/libril.so
 	rm /system/vendor/lib/libbrcm_ril_KYLEPRO.so
 	rm /system/vendor/lib/libril_KYLEPRO.so
 else
