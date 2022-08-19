447759f7b459557c292db6e6f82c7d579c3f420a
sostk <poudelswastik@gmail.com>
2022-08-19 11:26:13 +1000

    Update check_variant.sh

--- rootdir/check_variant.sh
+++ rootdir/check_variant.sh
@@ -10,16 +10,16 @@ then
 	sed -i -e 's/ro.product.device=kyleproxx/ro.product.device=kyleprods/g' /system/build.prop
 	sed -i -e 's/persist.radio.multisim.config=none/persist.radio.multisim.config=dsds/g' /system/build.prop
 	sed -i -e 's/ro.multisim.simslotcount=1/ro.multisim.simslotcount=2/g' /system/build.prop
-	mv /system/lib/libbrcm_ril_KYLEPRODS.so /system/lib/libbrcm_ril.so
-	mv /system/lib/libril_KYLEPRODS.so /system/lib/libril.so
-	rm /system/lib/libbrcm_ril_KYLEPRO.so
-	rm /system/lib/libril_KYLEPRO.so
+	mv /system/vendor/lib/libbrcm_ril_KYLEPRODS.so /system/lib/libbrcm_ril.so
+	mv /system/vendor/lib/libril_KYLEPRODS.so /system/lib/libril.so
+	rm /system/vendor/lib/libbrcm_ril_KYLEPRO.so
+	rm /system/vendor/lib/libril_KYLEPRO.so
 else
 	sed -i -e 's/ro.build.product=kyleproxx/ro.build.product=kylepro/g' /system/build.prop
 	sed -i -e 's/ro.product.model=GT-S758x/ro.product.model=GT-S7580/g' /system/build.prop
 	sed -i -e 's/ro.product.device=kyleproxx/ro.product.device=kylepro/g' /system/build.prop
-	mv /system/lib/libbrcm_ril_KYLEPRO.so /system/lib/libbrcm_ril.so
-	mv /system/lib/libril_KYLEPRO.so /system/lib/libril.so
-	rm /system/lib/libbrcm_ril_KYLEPRODS.so
-	rm /system/lib/libril_KYLEPRODS.so
+	mv /system/vendor/lib/libbrcm_ril_KYLEPRO.so /system/vendor/lib/libbrcm_ril.so
+	mv /system/vendor/lib/libril_KYLEPRO.so /system/vendor/lib/libril.so
+	rm /system/vendor/lib/libbrcm_ril_KYLEPRODS.so
+	rm /system/vendor/lib/libril_KYLEPRODS.so
 fi
