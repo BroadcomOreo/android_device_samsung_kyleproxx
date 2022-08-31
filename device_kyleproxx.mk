cef5d4abccf63e3e0ca9a264c69eabb17acc6b93
sostk <poudelswastik@gmail.com>
2022-08-31 14:53:04 +1000

    Update device_kyleproxx.mk

--- device_kyleproxx.mk
+++ device_kyleproxx.mk
@@ -132,9 +132,14 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
+
+    android.hardware.renderscript@1.0-impl 
+    
+# memtrack hawaii
+PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.renderscript@1.0-impl 
+    memtrack.hawaii
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
